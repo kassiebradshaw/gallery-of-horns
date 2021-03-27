849aaba1cd5905153d80ee72b74a2a237cbaf46a
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-03-27 16:24:00 -0700

    Completed Feature 1 Modal Box

---
+++ src/SelectedBeast.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Modal from 'react-bootstrap/Modal';
+import Card from 'react-bootstrap/Card';
+
+class SelectedBeast extends React.Component {
+  render() {
+    return (
+      <Modal show={this.props.displayModal} onHide={this.props.hideModal}>
+      <Modal.Dialog>
+        <Modal.Header closeButton />
+          <Modal.Body>
+          <Card style={{ width: '25rem'}}>
+            <Card.Img src={this.props.selectedBeast.image_url} />
+            <Card.Body>
+              <Card.Title>{this.props.selectedBeast.title}</Card.Title>
+              <Card.Text>{this.props.selectedBeast.description}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Modal.Body>
+      </Modal.Dialog>
+      </Modal>
+    )
+  }
+}
+
+export default SelectedBeast;
